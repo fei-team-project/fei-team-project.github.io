d771350daaa9a7f2d483b55a920eeb00bd195537
Michal Čopjan <michal@copjan.dev>
2024-10-07 14:24:17 +0200

    Fix wrong icon loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import './global.css'
-import Head from 'next/head'
 
 export const metadata: Metadata = {
     title: 'Tímový projekt'
@@ -9,12 +8,15 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang='en'>
-            <Head>
+            <head>
+                <link rel='icon' type='image/x-icon' href='favicon.ico' />
                 <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+                <link rel='icon' type='image/png' sizes='192x192' href='android-chrome-192x192.png' />
+                <link rel='icon' type='image/png' sizes='512x512' href='android-chrome-512x512.png' />
                 <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
                 <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
                 <link rel='manifest' href='/site.webmanifest' />
-            </Head>
+            </head>
             <body className='bg-background'>{children}</body>
         </html>
     )
