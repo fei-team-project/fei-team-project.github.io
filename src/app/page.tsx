f3717054f8867e9f7d5f06a1c74d1448915e047c
Michal Čopjan <michal@copjan.dev>
2024-10-13 13:37:51 +0200

    Edit home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,16 @@ export default function HomePage() {
     return (
         <div className='flex flex-col'>
             <TopBar />
-            <main className='flex flex-col items-center mb-5'>
+            <main className='pb-5 flex flex-col items-center'>
                 <h1 className='mt-40 text-5xl text-center'>Tímový projekt na FEI STU 2024/25</h1>
                 <h2 className='mt-10 mb-40 text-3xl text-center'>
                     Chatbot (GPT) na vyhľadávanie v štruktúrovaných dátach - agregovanie viacerých chatbotov
                 </h2>
                 <div className='flex justify-center items-center gap-5'>
-                    <Link href='https://github.com/orgs/fei-team-project/repositories'>
+                    <Link href='https://github.com/orgs/fei-team-project/repositories' target='_blank'>
                         <Image src='github-logo.svg' alt='Github logo' width='50' height='50' />
                     </Link>
-                    <Link href='https://trello.com/b/yrHQ2Hsf/tp-2024'>
+                    <Link href='https://trello.com/b/yrHQ2Hsf/tp-2024' target='_blank'>
                         <Image src='trello-logo.svg' alt='Trello logo' width='50' height='50' />
                     </Link>
                 </div>
