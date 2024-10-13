35627200e56c70f7c66b585606cbe58fa50cd999
Patrik Kaprinay <patrik.kaprinay@gmail.com>
2024-10-13 15:29:45 +0200

    fix: assignment bg shadow

--- src/app/assignment/page.tsx
+++ src/app/assignment/page.tsx
@@ -7,8 +7,8 @@ export default function AssignmentPage() {
             <TopBar />
             <main className='pt-24'>
                 <PageTitle title='Zadanie' />
-                <div className='sm:max-w-5xl mx-auto p-5 pt-0 sm:text-lg  rounded'>
-                    <div className='flex flex-col gap-3 bg-white px-6 py-5'>
+                <div className='sm:max-w-5xl mx-auto p-5 pt-0 sm:text-lg'>
+                    <div className='flex flex-col gap-3 bg-white px-6 py-5 rounded shadow'>
                         <p>
                             Vytvorte webovú aplikáciu, chatbot, ktorý bude vyhľadávať polovodičové súčiastky na základe dotazov
                             formulovaných voľným textom.
