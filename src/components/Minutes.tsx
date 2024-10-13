2dd54b33ce0e18e3c71a638341d9051ebc1595cd
Patrik Kaprinay <patrik.kaprinay@gmail.com>
2024-10-13 15:14:44 +0200

    feat: added text on tile hovers

--- src/components/Minutes.tsx
+++ src/components/Minutes.tsx
@@ -6,11 +6,15 @@ export default function Minutes({ date, path }: { date: string; path: string }) 
             href={path ? path : '#'}
             target='_blank'
             className='min-h-20 bg-white hover:bg-gray-50 rounded shadow flex justify-center items-center text-center sm:text-lg transition duration-100'
+            title='Otvoriť zápisnicu'
         >
             {date}
         </Link>
     ) : (
-        <div className='min-h-20 bg-white rounded shadow flex justify-center items-center text-center sm:text-lg bg-white/40'>
+        <div
+            className='min-h-20 bg-white rounded shadow flex justify-center items-center text-center sm:text-lg bg-white/40'
+            title='Stretnutie sa ešte neuskutočnilo'
+        >
             {date}
         </div>
     )
