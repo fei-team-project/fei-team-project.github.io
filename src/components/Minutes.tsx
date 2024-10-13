f241ae72a16d26ef9f6ea74e1a1b7186273e1051
Michal Čopjan <michal@copjan.dev>
2024-10-13 13:47:38 +0200

    Edit minutes page

--- src/components/Minutes.tsx
+++ src/components/Minutes.tsx
@@ -4,6 +4,7 @@ export default function Minutes({ date, path }: { date: string; path: string }) 
     return (
         <Link
             href={path}
+            target='_blank'
             className='min-h-20 bg-white hover:bg-gray-50 rounded shadow flex justify-center items-center text-center sm:text-lg'
         >
             {date}
