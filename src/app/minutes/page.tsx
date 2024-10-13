bfc236f082ec3aa3f9491286e4fe5773709bad76
Michal Čopjan <michal@copjan.dev>
2024-10-13 11:00:10 +0200

    Add minutes page

---
+++ src/app/minutes/page.tsx
@@ -0,0 +1,45 @@
+import Minutes from '@/components/Minutes'
+import PageTitle from '@/components/PageTitle'
+import TopBar from '@/components/TopBar'
+
+export default function MinutesPage() {
+    const minutes = [
+        { id: '1', date: '30.09.2024', path: '/minutes/minutes-01-2024-09-30.pdf' },
+        { id: '2', date: '7.10.2024', path: '/minutes/minutes-02-2024-10-07.pdf' },
+        { id: '3', date: '', path: '' },
+        { id: '4', date: '', path: '' },
+        { id: '5', date: '', path: '' },
+        { id: '6', date: '', path: '' },
+        { id: '7', date: '', path: '' },
+        { id: '8', date: '', path: '' },
+        { id: '9', date: '', path: '' },
+        { id: '10', date: '', path: '' },
+        { id: '11', date: '', path: '' },
+        { id: '12', date: '', path: '' },
+        { id: '13', date: '', path: '' },
+        { id: '14', date: '', path: '' },
+        { id: '15', date: '', path: '' },
+        { id: '16', date: '', path: '' },
+        { id: '17', date: '', path: '' },
+        { id: '18', date: '', path: '' },
+        { id: '19', date: '', path: '' },
+        { id: '20', date: '', path: '' },
+        { id: '21', date: '', path: '' },
+        { id: '22', date: '', path: '' },
+        { id: '23', date: '', path: '' },
+        { id: '24', date: '', path: '' }
+    ]
+    return (
+        <div>
+            <TopBar />
+            <main className='pt-24'>
+                <PageTitle title='Zápisnice' />
+                <div className='max-w-5xl mx-auto pl-5 pr-5 pb-5 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-5'>
+                    {minutes.map((minutes) => (
+                        <Minutes key={minutes.id} date={minutes.date} path={minutes.path} />
+                    ))}
+                </div>
+            </main>
+        </div>
+    )
+}
---
+++ src/components/Minutes.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function Minutes({ date, path }: { date: string; path: string }) {
+    return (
+        <Link
+            href={path}
+            className='min-h-16 p-1 bg-white hover:bg-gray-50 rounded shadow flex justify-center items-center text-center sm:text-lg'
+        >
+            {date}
+        </Link>
+    )
+}
