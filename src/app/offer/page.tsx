5d4d2a8eb2acf6527c3ef51f821217e133d7169f
Patrik Kaprinay <patrik.kaprinay@gmail.com>
2024-10-13 15:07:13 +0200

    fix: page width to fit other subpage widths

--- src/app/offer/page.tsx
+++ src/app/offer/page.tsx
@@ -9,7 +9,7 @@ export default function OfferPage() {
             <TopBar />
             <main className='pt-24'>
                 <PageTitle title='Ponuka' />
-                <div className='max-w-5xl mx-auto pl-3 pr-3 mb-3 flex flex-col gap-3'>
+                <div className='max-w-5xl mx-auto px-5 mb-3 flex flex-col gap-3'>
                     <Image src='/offer-01.jpg' alt='Offer 1/4' width='100' height='100' className='w-full shadow' />
                     <Image src='/offer-02.jpg' alt='Offer 2/4' width='100' height='100' className='w-full shadow' />
                     <Image src='/offer-03.jpg' alt='Offer 3/4' width='100' height='100' className='w-full shadow' />
