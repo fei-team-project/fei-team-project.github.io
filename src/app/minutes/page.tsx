8576d6bbb16f1c13e602f37006715fe9079a30a3
Patrik Kaprinay <patrik.kaprinay@gmail.com>
2024-10-14 13:16:34 +0200

    fix: auto-tiling; no need for empty tiles

--- src/app/minutes/page.tsx
+++ src/app/minutes/page.tsx
@@ -6,28 +6,7 @@ export default function MinutesPage() {
     const minutes = [
         { date: '30.09.2024', path: '/minutes/minutes-01-2024-09-30.pdf' },
         { date: '7.10.2024', path: '/minutes/minutes-02-2024-10-07.pdf' },
-        { date: '', path: '' },
-        { date: '', path: '' },
-        { date: '', path: '' },
-        { date: '', path: '' },
-        { date: '', path: '' },
-        { date: '', path: '' },
-        { date: '', path: '' },
-        { date: '', path: '' },
-        { date: '', path: '' },
-        { date: '', path: '' },
-        { date: '', path: '' },
-        { date: '', path: '' },
-        { date: '', path: '' },
-        { date: '', path: '' },
-        { date: '', path: '' },
-        { date: '', path: '' },
-        { date: '', path: '' },
-        { date: '', path: '' },
-        { date: '', path: '' },
-        { date: '', path: '' },
-        { date: '', path: '' },
-        { date: '', path: '' }
+        { date: '14.10.2024', path: '' }
     ]
     return (
         <div>
@@ -39,11 +18,17 @@ export default function MinutesPage() {
                         {minutes.slice(0, 12).map((minutes, i) => (
                             <Minutes key={i} date={minutes.date} path={minutes.path} />
                         ))}
+                        {Array.from({ length: 12 - minutes.length }).map((_, i) => (
+                            <Minutes key={minutes.length + i} date='' path='' />
+                        ))}
                     </div>
                     <hr className='w-full' />
                     <div className='px-5 pb-5 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-5'>
                         {minutes.slice(12, 24).map((minutes, i) => (
-                            <Minutes key={i} date={minutes.date} path={minutes.path} />
+                            <Minutes key={'2' + i} date={minutes.date} path={minutes.path} />
+                        ))}
+                        {Array.from({ length: 12 - Math.min(12, minutes.slice(12, 24).length) }).map((_, i) => (
+                            <Minutes key={'2' + (minutes.slice(12, 24).length + i)} date='' path='' />
                         ))}
                     </div>
                 </div>
