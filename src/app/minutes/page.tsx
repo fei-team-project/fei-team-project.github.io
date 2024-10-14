ba5bd50b97ad73b95833c708e914921195738b78
Patrik Kaprinay <patrik.kaprinay@gmail.com>
2024-10-14 13:09:28 +0200

    feat: minutes layout

--- src/app/minutes/page.tsx
+++ src/app/minutes/page.tsx
@@ -4,28 +4,48 @@ import TopBar from '@/components/TopBar'
 
 export default function MinutesPage() {
     const minutes = [
-        { id: '1', date: '30.09.2024', path: '/minutes/minutes-01-2024-09-30.pdf' },
-        { id: '2', date: '7.10.2024', path: '/minutes/minutes-02-2024-10-07.pdf' },
-        { id: '3', date: '', path: '' },
-        { id: '4', date: '', path: '' },
-        { id: '5', date: '', path: '' },
-        { id: '6', date: '', path: '' },
-        { id: '7', date: '', path: '' },
-        { id: '8', date: '', path: '' },
-        { id: '9', date: '', path: '' },
-        { id: '10', date: '', path: '' },
-        { id: '11', date: '', path: '' },
-        { id: '12', date: '', path: '' }
+        { date: '30.09.2024', path: '/minutes/minutes-01-2024-09-30.pdf' },
+        { date: '7.10.2024', path: '/minutes/minutes-02-2024-10-07.pdf' },
+        { date: '', path: '' },
+        { date: '', path: '' },
+        { date: '', path: '' },
+        { date: '', path: '' },
+        { date: '', path: '' },
+        { date: '', path: '' },
+        { date: '', path: '' },
+        { date: '', path: '' },
+        { date: '', path: '' },
+        { date: '', path: '' },
+        { date: '', path: '' },
+        { date: '', path: '' },
+        { date: '', path: '' },
+        { date: '', path: '' },
+        { date: '', path: '' },
+        { date: '', path: '' },
+        { date: '', path: '' },
+        { date: '', path: '' },
+        { date: '', path: '' },
+        { date: '', path: '' },
+        { date: '', path: '' },
+        { date: '', path: '' }
     ]
     return (
         <div>
             <TopBar />
             <main className='pt-24'>
                 <PageTitle title='Zápisnice' />
-                <div className='max-w-5xl mx-auto pl-5 pr-5 pb-5 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-5'>
-                    {minutes.map((minutes) => (
-                        <Minutes key={minutes.id} date={minutes.date} path={minutes.path} />
-                    ))}
+                <div className='max-w-5xl mx-auto flex flex-col gap-6'>
+                    <div className='px-5 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-5'>
+                        {minutes.slice(0, 12).map((minutes, i) => (
+                            <Minutes key={i} date={minutes.date} path={minutes.path} />
+                        ))}
+                    </div>
+                    <hr className='w-full' />
+                    <div className='px-5 pb-5 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-5'>
+                        {minutes.slice(12, 24).map((minutes, i) => (
+                            <Minutes key={i} date={minutes.date} path={minutes.path} />
+                        ))}
+                    </div>
                 </div>
             </main>
         </div>
