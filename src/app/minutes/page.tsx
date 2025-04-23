a151cdcfcc44f9c1707aee3a311afff3e0404083
Lubos Sekerak <lubos.sekerak@webgang.io>
2025-04-23 11:45:23 +0200

    add minutes

--- src/app/minutes/page.tsx
+++ src/app/minutes/page.tsx
@@ -23,6 +23,7 @@ export default function MinutesPage() {
         { date: '24.03.2025', path: '/minutes/minutes-2025-03-24.pdf' },
         { date: '02.04.2025', path: '/minutes/minutes-2025-04-02.pdf' },
         { date: '09.04.2025', path: '/minutes/minutes-2025-04-09.pdf' },
+        { date: '16.04.2025', path: '/minutes/minutes-2025-04-16.pdf' },
     ]
 
     return (
